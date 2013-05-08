c95be82af90c7837546ca216dc142202ca359d3e
unknown <vttaneja@SEA-6900098566.ant.amazon.com>
2013-05-08 01:24:53 -0700

    Adding empty class for "tracing outer nodes" algorithm problem

---
+++ AlgoQuestions/TraceOuterNodes.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AlgoQuestions
+{
+	/// <summary>
+	/// Given a binary tree, print the outer nodes of the tree.
+	/// </summary>
+	public class TraceOuterNodes
+	{
+		public TraceOuterNodes ()
+		{
+		}
+	}
+}
+
